99c310ed19580e5ac9cac6b201da21f5911e2d18
Ahmed Hamid <ahmadhamidty@gmail.com>
2020-05-05 00:55:28 +0200

    Simple Project Sum of the two number for SecondProject

---
+++ SecondSimpleProject.cpp
@@ -0,0 +1,22 @@
+#include <iostream>	
+
+
+int main () 	
+	
+{	
+
+int num1 =0 , num2 = 0 , sum =0 ;
+
+cout << " Please Enter your first number " << endl;
+cin >> x ; 
+
+cout << " Please Enter your Second number : " << endl;
+cin >> y ;
+
+sum = num1 + num2;
+
+count << " The sum of the two number is :  " << sum << endl;
+
+
++return 0;	
+{
