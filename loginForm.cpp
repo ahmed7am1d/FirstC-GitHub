395276f153dfe4307c616e5e76695c913a56dc92
Ahmed Hamid <ahmadhamidty@gmail.com>
2020-05-05 01:46:43 +0200

    C++ LoginForm Project

---
+++ loginForm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>	
+
+
+int main () 	
+	
+{	
+
+string UserName =0 , Password = 0 , Pin =0 ;
+
+cout << " Please Enter your UserName " << endl;
+cin >> UserName ; 
+
+cout << " Please Enter your Password : " << endl;
+cin >> Password ;
+
+
+cout << " Please Enter your Pin : " << endl;
+cin >> Pin ;
+
+
++return 0;	
+{
